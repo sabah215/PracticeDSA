a3fed2fc1ebec64a2c477354bc777570b43dd1fe
Sabah Ummie <sabah.ummie@gmail.com>
2022-05-31 18:36:00 -0400

    rotatedBS undone

--- src/main/java/com/recursion/RecursionWithArray.java
+++ src/main/java/com/recursion/RecursionWithArray.java
@@ -2,21 +2,26 @@ package com.recursion;
 
 import javax.xml.transform.stax.StAXResult;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class RecursionWithArray {
 
     public static void main(String[] args) {
-        int [] arr = {1,2,4,8,8,9,12};
-    ArrayList<Integer> list = new ArrayList<>();
+        int [] arr = {1,2,4,8,9,9,12, 9};
+
+        ArrayList<Integer> list =
+                findIndexes(arr, 9,0,new ArrayList<>());
 
 //        System.out.println(isSorted(arr,0));
 //        System.out.println(search(arr,8,0));
 //        System.out.println(findIndex(arr,8,0));
 //        System.out.println(findIndexLast(arr,8,arr.length-1));
-        System.out.println(findIndexes(arr, 8,0,list));
+        System.out.println(list);
+
     }
 
-    public ArrayList<Integer> findIndexes(int[] arr, int val, int index, ArrayList<Integer> list){
+    public static ArrayList<Integer> findIndexes(int[] arr, int val, int index, ArrayList<Integer> list){
          if(index == arr.length)
              return list;
 
@@ -24,6 +29,17 @@ public class RecursionWithArray {
              list.add(index);
 
          return findIndexes(arr, val,index+1,list);
+    }
+
+    public static ArrayList<Integer> findIndexes2(int[] arr, int val, int index){
+        ArrayList<Integer> list = new ArrayList<>();
+        if(index == arr.length)
+            return list;
+
+        if (val == arr[index])
+            list.add(index);
+
+        return findIndexes(arr, val,index+1,list);
     }
     // check if sorted
     public static boolean isSorted(int[] arr, int index){
---
+++ src/main/java/com/recursion/rotatedBinarySearch.java
@@ -0,0 +1,11 @@
+package com.recursion;
+
+public class rotatedBinarySearch {
+    public static void main(String[] args) {
+        int [] arr = {5,6,7,8,9,1,2,3};
+
+
+    }
+
+
+}
