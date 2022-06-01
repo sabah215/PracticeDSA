940d25110ef5153b44245277ef33677801f250d9
Sabah Ummie <sabah.ummie@gmail.com>
2022-05-31 20:57:16 -0400

    RReverse Polish notation incomplete

---
+++ src/main/java/com/leetcode/ReversePolishNotation.java
@@ -0,0 +1,34 @@
+package com.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ReversePolishNotation {
+
+    List<Character> list = new LinkedList<Character>();
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    * @TODO: Start Here
+    *
+    * */
+    public static int evalRPN(String [] tokens){
+        int result = 0;
+
+       for (int i=0; i< tokens.length; i++){
+           int a;
+           int b;
+
+
+
+
+       }
+
+        return result;
+    }
+
+
+}
