650c77a4b5cf6a907b5a432cd21bd46e7392d946
Sabah Ummie <sabah.ummie@gmail.com>
2022-06-11 11:34:01 -0400

    prformed merge sort and a recursion problem

---
+++ src/main/java/com/recursion/geeksforgeeks/ConstructTriangle.java
@@ -0,0 +1,23 @@
+package com.recursion.geeksforgeeks;
+
+import java.util.Arrays;
+
+public class ConstructTriangle {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        printTriangle(arr);
+    }
+    public static void printTriangle(int [] A){
+
+        if(A.length < 1)
+            return;
+
+        int [] temp = new int [A.length -1];
+
+        for(int i=0; i< A.length -1; i++){
+            temp[i] = A[i] + A[i+1];
+        }
+        printTriangle(temp);
+        System.out.println(Arrays.toString(A));
+    }
+}
--- src/main/java/com/recursion/sorting/MergeSort.java
+++ src/main/java/com/recursion/sorting/MergeSort.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int [] arr = {5,4,6,2,1,8,10,7};
-        System.out.println(Arrays.toString(mergeSort(arr)));
+        int [] arr = {4,6,2,8,10,7};
+
+        mergeSortInPlace(arr, 0, arr.length);
+        System.out.println(Arrays.toString(arr));
     }
 
     static int [] mergeSort(int [] arr){
@@ -50,5 +52,52 @@ public class MergeSort {
             k++;
         }
         return mix;
+    }
+
+    static void mergeSortInPlace(int [] arr, int s, int e){
+        if(e - s == 1){
+            return;
+        }
+        int mid = (s + e)/2;
+        mergeSortInPlace(arr,s,mid);
+        mergeSortInPlace(arr,mid,e);
+
+         mergeInPlace(arr, s, mid, e);
+    }
+
+
+    private  static void mergeInPlace(int[] arr, int s, int m, int e){
+        int [] mix =  new int[e - s];
+
+        int i=s;
+        int j=m;
+        int k=0;
+
+        while(i < m && j < e){
+            if(arr[i] < arr[j]){
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+        // One of the arrays is incomplete
+        // add all elements in the element.
+        while(i < m){
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+        while(j < e){
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+        for (int l = 0; l < mix.length; l++) {
+            arr[s+l] = mix[l];
+        }
+
     }
 }
