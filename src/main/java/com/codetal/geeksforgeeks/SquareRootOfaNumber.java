2da0e77f6dc5da2dac7d3e1f7ed97abbf7ecf253
Sabah Ummie <sabah.ummie@gmail.com>
2022-08-15 11:13:34 -0400

    square root of a number calculated.

--- src/main/java/com/codetal/geeksforgeeks/SquareRootOfaNumber.java
+++ src/main/java/com/codetal/geeksforgeeks/SquareRootOfaNumber.java
@@ -7,6 +7,35 @@ then return floor(√x).
 * */
 public class SquareRootOfaNumber {
     public static void main(String[] args) {
+        long ans = floorSqrt(17);
+        System.out.println(ans);
+    }
+
+    /* Steps:
+     * 1. Notice that with the increasing sqrt values, perfect square values are also increasing
+     * 2. The square root values are always <= x/2
+     * */
+    static long floorSqrt(long x)
+    {
+        // Your code here
+        if(x==1) return 1;
+        long s = 1;
+        long e = x/2;
+        long ans= 0;
+        while(s <= e){
+            long m = s+(e-s)/2;
+            long sqr = m * m;
+            if(sqr == x)
+               return m;
+            else if(sqr < x){
+               ans = m;
+               s = m+1;
+            }
+            else {
+                e = m-1;
+            }
+        }
+        return ans;
 
     }
 }
