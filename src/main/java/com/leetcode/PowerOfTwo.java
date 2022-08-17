49c15bdff3a78855af2a0707ec08cb13f9cf5ca8
Sabah Ummie <sabah.ummie@gmail.com>
2022-08-17 17:21:04 -0400

    Power of Two solved.

---
+++ src/main/java/com/leetcode/PowerOfTwo.java
@@ -0,0 +1,18 @@
+package com.leetcode;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfTwo(-16));
+//         System.out.println(-16%2);
+    }
+
+    static boolean isPowerOfTwo(int n) {
+
+            if (n == 0) return false;
+            if (n == 1) return true;
+            // here negative values are excluded
+            if(n % 2 != 0) return false;
+            return isPowerOfTwo(n/2);
+    }
+
+}
