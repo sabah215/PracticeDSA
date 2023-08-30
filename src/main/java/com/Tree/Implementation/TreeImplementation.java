a2614046ce6a1f92f580642070a15201a77df159
sabah215 <sabah.ummie@gmail.com>
2023-08-30 02:16:24 -0400

    Tree Implementation from KK

--- src/main/java/com/Tree/Implementation/TreeImplementation.java
+++ src/main/java/com/Tree/Implementation/TreeImplementation.java
@@ -1,5 +1,7 @@
 package com.Tree.Implementation;
 
+import java.util.Scanner;
+
 public class TreeImplementation {
 
     public TreeImplementation(){
@@ -19,5 +21,29 @@ public class TreeImplementation {
     private Node root;
 
     // insert elements
+    public void populate(Scanner scanner){
+        System.out.println("Enter the root node: ");
+        int value = scanner.nextInt();
+        root = new Node(value);
+        populate(scanner, root);
+    }
 
+    private void populate(Scanner scanner, Node node){
+        System.out.println("Do you want to enter left of "+ node.value);
+        boolean isLeft = scanner.nextBoolean();
+        if(isLeft){
+            System.out.println("Enter the value");
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner, node.left);
+        }
+        System.out.println("Do you want to enter right of "+ node.value);
+        boolean isRight = scanner.nextBoolean();
+        if(isRight){
+            System.out.println("Enter the value");
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            populate(scanner, node.right);
+        }
+    }
 }
